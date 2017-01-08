232a89a1889314de46cdb86906727a0253fc739a
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2017-01-09 00:19:10 +1100

    Added Navbar

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ProfileComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   bootstrap:    [ AppComponent ]
 })
---
+++ app/components/projects/projects.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'projects',
+  templateUrl: `projects.component.html`,
+})
+export class ProjectsComponent  { name = 'Angular'; }
